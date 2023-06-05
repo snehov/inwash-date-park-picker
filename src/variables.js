fca7cb9233ca6fbd17028ffb4674110c5f1ff207
snehov <snehov@gmail.com>
2023-06-05 12:59:51 +0000

    change EXTRA_DAY_PARK to 200

--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,4 @@
 export const dateFormat = "d.M.yyyy HH:mm";
 export const dateJsonFormat = "yyyy-MM-dd";
 export const sendDateFormat = "dd.MM.yyyy HH:mm";
-export const EXTRA_DAY_PARK = 130;
+export const EXTRA_DAY_PARK = 200;
